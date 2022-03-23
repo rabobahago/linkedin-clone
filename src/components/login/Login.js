ab036076c215269160a0758ba93484a8ff028f4c
rabobahago <rabobahago@gmail.com>
2022-03-23 21:45:14 +0100

    login styling added

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -7,7 +7,10 @@ const Login = () => {
         <a href="/">
           <img src="/image/login-logo.svg" alt="" />
         </a>
-        <Join>Join Now</Join>
+        <div>
+          <Join>Join Now</Join>
+          <Signin>Sign in</Signin>
+        </div>
       </Nav>
     </Container>
   );
@@ -19,6 +22,7 @@ const Nav = styled.nav`
   max-width: 1128px;
   margin: auto;
   padding: 12px 0 16px;
+  font-weight: bold;
   display: flex;
   justify-content: space-between;
   position: relative;
@@ -32,5 +36,39 @@ const Nav = styled.nav`
     }
   }
 `;
-const Join = styled.a``;
+const Join = styled.a`
+  padding: 10px 12px;
+  font-size: 16px;
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.8);
+    text-decoration: none;
+    cursor: pointer;
+    outline: 0.5px solid rgba(0, 0, 0, 0.6);
+  }
+`;
+const Signin = styled.a`
+  box-sizing: insert 0 0 0 1px #0a66c2;
+  padding: 10px 24px;
+  font-size: 16px;
+  text-decoration: none;
+  transition-duration: 167ms;
+  outline: 0.5px solid #0e76a8;
+  text-align: center;
+  font-weight: 600;
+  color: #0e76a8;
+  line-height: 40px;
+  margin: 5px;
+  border-radius: 24px;
+  &:hover {
+    cursor: pointer;
+    background-color: rgba(112, 181, 249, 0.15);
+    color: #0a66c2;
+    text-decoration: none;
+  }
+`;
+
 export default Login;
